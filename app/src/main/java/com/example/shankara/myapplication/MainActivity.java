301eb097b1beb6fec30104b82a590309d5ea603f
Shankara <shankara.vandeven@student.uva.nl>
2018-02-08 11:54:54 +0100

    Added animation to Mr. Potatohead

--- app/src/main/java/com/example/shankara/myapplication/MainActivity.java
+++ app/src/main/java/com/example/shankara/myapplication/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.action_bar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
     }
 
     /** Save checkbox statuses in saveInstanceState */
